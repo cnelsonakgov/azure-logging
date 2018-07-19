0c1c342642481d01935c64f48bf354178df156da
Nelson, Curtis L <curtis.nelson@alaska.gov>
2018-07-18 16:04:30 -0800

    Add new console logging app

---
+++ console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Serilog;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Serilog logger
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Trace()
+                .CreateLogger();
+            
+            try
+            {
+                Log.Information("Application starting ...");
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Terminated with exception");
+            }
+            finally
+            {
+                Log.Information("Application closing ...");
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
