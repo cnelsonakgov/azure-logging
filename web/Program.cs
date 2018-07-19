dc3c11e8fa5d3fa0c0b7184689758fc5b6cae028
Nelson, Curtis L <curtis.nelson@alaska.gov>
2018-07-18 16:05:10 -0800

    Move and rename web logging app

--- web/Program.cs
+++ web/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole;
